cf98c49be3ee51b78d9d6430e254c9ae7ec4d913
Liju <liju@Lijus-MacBook-Air.local>
2023-09-09 16:49:47 +0530

    creating header component

--- urban-fishes/src/App.js
+++ urban-fishes/src/App.js
@@ -4,19 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className='header-content'>
+          <h1>Urban Fish Company</h1>
+          <div className='header-line'></div>
+          <h3 className='header-description'>
+            The perfect place for the sea fish
+          </h3>
+        </div>
       </header>
     </div>
   );
